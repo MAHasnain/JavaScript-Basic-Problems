7e1c852e037a77ff0b5a4e0678a3e9c2bbbfa9ce
Atta Hasnain <attaulhasnainmaqsoodi@gmail.com>
2023-09-22 11:05:10 +0500

    problem 2 solved

---
+++ problem 2.mjs
@@ -0,0 +1,14 @@
+
+// 2. Write a function that takes two numbers as arguments and returns their sum.
+
+function prblm(num1, num2) {
+    return 13 + 32
+}
+console.log(prblm());
+
+
+
+function prblm2(num2, num1) {
+    return ("MA" + " " + "Hasnain")
+}
+console.log(prblm2())
