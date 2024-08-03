eb8f40022d56e67a0cff095196f34380d0eed984
MAHasnain <attaulhasnainmaqsoodi@gmail.com>
2024-08-03 10:42:26 +0500

    deleted

--- problem 1.mjs
+++
@@ -1,7 +0,0 @@
-// 1. Create a variable and store your name in it. Then, use `console.log()` to print it to the console.
-
-const myName = "Muhammad Atta ul Hasnain";
-const myFatherName = "Muhammad Azam Khan";
-
-console.log(myName);
-console.log(myFatherName);
